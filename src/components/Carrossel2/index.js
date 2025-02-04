6503883f05cff33d051dc0ccfd6aa29615f1320e
MayraLeticia <mayraleticia03@gmail.com>
2025-02-04 15:02:09 -0300

    :sparkles:fix: Carrossel perfeito

--- src/components/Carrossel2/index.js
+++ src/components/Carrossel2/index.js
@@ -1,14 +1,14 @@
 "use client";
 
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import styles from './style.module.scss';
 
 const images = [
   "/images/aniversario.png",
-  "/images/exemplo1.png",
-  "/images/exemplo2.png"
+  "/images/aniversario.png",
+  "/images/aniversario.png"
 ];
 
 export default function Carousel() {
@@ -23,21 +23,24 @@ export default function Carousel() {
   };
 
   return (
-    <div className="carousel-container">
-      <motion.img
-        key={index}
-        src={images[index]}
-        alt="Carousel"
-        className="carousel-image"
-        initial={{ opacity: 0, x: 50 }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: -50 }}
-        transition={{ duration: 0.5 }}
-      />
-      <button className="carousel-button left" onClick={prevSlide}>
+    <div className={styles.carouselContainer}>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={index}
+          className={styles.carouselImageWrapper}
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -50 }}
+          transition={{ duration: 0.5 }}
+        >
+          <img src={images[index]} alt="Carousel" className={styles.carouselImage} />
+        </motion.div>
+      </AnimatePresence>
+
+      <button className={`${styles.carouselButton} ${styles.left}`} onClick={prevSlide}>
         <ChevronLeft />
       </button>
-      <button className="carousel-button right" onClick={nextSlide}>
+      <button className={`${styles.carouselButton} ${styles.right}`} onClick={nextSlide}>
         <ChevronRight />
       </button>
     </div>
