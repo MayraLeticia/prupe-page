03c8e03d1fd4a9a9b0ab7afa44c6ff44589f0987
MayraLeticia <mayraleticia03@gmail.com>
2025-02-04 16:37:27 -0300

    :ok_hand::feat: links e noticias

--- src/components/Carrossel2/index.js
+++ src/components/Carrossel2/index.js
@@ -7,8 +7,14 @@ import styles from './style.module.scss';
 
 const images = [
   "/images/aniversario.png",
-  "/images/aniversario.png",
-  "/images/aniversario.png"
+  "/images/noticia2.png",
+  "/images/noticia3.png"
+];
+
+const links = [
+  "https://www.instagram.com/prupepodologia/",  // Link da primeira imagem
+  "https://g1.globo.com/ce/ceara/especial-publicitario/sistema-fecomercio/radar-do-comercio/noticia/2020/02/28/cuidado-com-os-pes-mais-do-que-beleza-uma-questao-de-saude.ghtml",  // Link da segunda imagem
+  "https://blogs.opovo.com.br/radardocomercio/2020/02/24/podologia-o-que-seus-pes-dizem-sobre-sua-saude/"   // Link da terceira imagem
 ];
 
 export default function Carousel() {
@@ -22,6 +28,10 @@ export default function Carousel() {
     setIndex((prev) => (prev - 1 + images.length) % images.length);
   };
 
+  const handleImageClick = () => {
+    window.open(links[index], "_blank"); // Abre o link em uma nova aba
+  };
+
   return (
     <div className={styles.carouselContainer}>
       <AnimatePresence mode="wait">
@@ -33,7 +43,13 @@ export default function Carousel() {
           exit={{ opacity: 0, x: -50 }}
           transition={{ duration: 0.5 }}
         >
-          <img src={images[index]} alt="Carousel" className={styles.carouselImage} />
+          <img
+            src={images[index]}
+            alt="Carousel"
+            className={styles.carouselImage}
+            onClick={handleImageClick} // Adiciona a função de clique
+            style={{ cursor: "pointer" }} // Garante que o cursor mude para indicar um link
+          />
         </motion.div>
       </AnimatePresence>
 
